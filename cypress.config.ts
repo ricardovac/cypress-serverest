be45a96159e63834fbd54bf5984a65fd8932733d
ricardovac <ricardoviicius@gmail.com>
2023-11-14 18:58:03 -0300

    feat copy videos to public folder

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporter-config.json",
+  },
   e2e: {
-    baseUrl: 'https://serverest.dev',
+    baseUrl: "https://serverest.dev",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
